f02f50d6c32b30b035861d10f251308cb4745172
Trevaughn McGregor <SIRGHST@users.noreply.github.com>
2016-03-13 17:17:19 -0400

    Added files via upload

---
+++ Project1/Project1/project1_1.3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int C;  //store credit
+	int arr[100];
+	int T,I;
+	cin >> T;
+	
+	
+	for(int testcase = 1; testcase<= T; testcase++ ){
+		cin >> C;
+		cin >> I;
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	return 0;
+}
